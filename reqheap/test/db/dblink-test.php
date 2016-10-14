eb43cd6d1785f7e6dbc3c754c8d8233208432225
asiaxing <asiaxing@163.com>
2016-10-14 08:57:32 +0800

    update dblink test

--- reqheap/test/db/dblink-test.php
+++ reqheap/test/db/dblink-test.php
@@ -8,6 +8,7 @@
 		$ret = false;
 		
 		$host = 'localhost';
+		
 		$super_user = 'root';
 		$super_user_password = '1234';
 		
@@ -15,51 +16,70 @@
 		$test_user = 'test_user';
 		$test_user_password = '123_abc_ABC';
 		
-		$link = new dblink($host, $super_user, $super_user_password);
+		$super_dblink = new dblink($host, $super_user, $super_user_password);
 		
-		// create test_database
+		// create test database
 		$sql = "create database if not exists $test_database";
-		$ret = $link->exec_sql($sql);
+		$ret = $super_dblink->exec_sql($sql);
 		
 		if($ret)
 		{
-			// create test_user
+			// create test user
 			$sql = "grant all privileges on $test_database.* to $test_user@$host identified by '$test_user_password'";
-			$ret = $link->exec_sql($sql);
+			$ret = $super_dblink->exec_sql($sql);
+			
+			if($ret)
+			{
+				$test_dblink = new dblink($host, $test_user, $test_user_password, $test_database);
+		
+				// create table
+				$sql = "create table if not exists `user`(
+					`id` int(11) not null auto_increment, 
+					`name` varchar(255) not null, 
+					`password` varchar(255) not null, 
+					primary key(`id`)) 
+					engine=myisam 
+					default charset=utf8 
+					auto_increment=2";
+				$ret = $test_dblink->exec_sql($sql);
+				
+				if($ret)
+				{
+					// drop test database
+					$sql = "drop database if exists $test_database";
+					$ret = $super_dblink->exec_sql($sql);
+					
+					if($ret)
+					{
+						// drop test user
+						$sql = "drop user $test_user@$host";
+						$ret = $super_dblink->exec_sql($sql);
+					}
+				}
+			}
 		}
 		
 		return $ret;
 	}
 
-	function dblink_test_case_02()
-	{
-		$ret = false;
-		
-		$host = 'localhost';
-		$test_database = 'test';
-		$test_user = 'test_user';
-		$test_user_password = '123_abc_ABC';
-		
-		$link = new dblink($host, $test_user, $test_user_password, $test_database);
-		
-		// create table
-		$sql = "create table if not exists `user`(`id` int(11) not null auto_increment, `name` varchar(255) not null, `password` varchar(255) not null, primary key(`id`)) engine=myisam default charset=utf8 auto_increment=2";
-		$ret = $link->exec_sql($sql);
-		return $ret;
-	}
 ?>
 
 <?php
 	// run test
-	print "<br>===============================================<br>";
-	if(dblink_test_case_01())
-		print "[dblink_test_case_01] test result: pass<br>";
+	print "====================<br>";
+	print "DBLINK TEST CASE 01<br>";
+	print "====================<br>";
+	$ret = dblink_test_case_01();
+	if($ret)
+	{
+		print "==========<br>";
+		print "PASS<br>";
+		print "==========<br>";
+	}
 	else
-		print "[dblink_test_case_01] test result: fail<br>";
-	
-	print "<br>===============================================<br>";
-	if(dblink_test_case_02())
-		print "[dblink_test_case_02] test result: pass<br>";
-	else
-		print "[dblink_test_case_02] test result: fail<br>";
+	{
+		print "==========<br>";
+		print "FAIL<br>";
+		print "==========<br>";
+	}
 ?>
