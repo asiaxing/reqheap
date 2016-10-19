12c8db6531ab09c08ef2e341382092cc5a8e1520
asiaxing <asiaxing@163.com>
2016-10-19 14:24:15 +0800

    update db-config

--- reqheap/db/db-config.php
+++ reqheap/db/db-config.php
@@ -1,9 +1,9 @@
 <?php
-   
     
     class IniFile
     {
         private $file_name;
+        
         public function __construct($file_name)
         {
             $this->file_name = $file_name;
@@ -11,8 +11,8 @@
         
         public function write($key, $value)
         {
-            $record = "$key = $value";
-            file_put_contents($this->file_name, $record);
+            $record = "$key = $value\n";
+            file_put_contents($this->file_name, $record, FILE_APPEND);
         }
         
         public function read($key)
@@ -22,7 +22,19 @@
         }
     }
     
+
     $cfg_file = new IniFile('config.ini');
-    $cfg_file->write('host', 'localhost');
+    
+    $cfg_file->write("host", "localhost");
+    $cfg_file->write("app_user", "app_user");
+    $cfg_file->write("app_user_password", "123_abc_ABC");
+    $cfg_file->write("app_database", "app_database");
+    
     print $cfg_file->read('host');
+    print "<br>";
+    print $cfg_file->read('app_user');
+    print "<br>";
+    print $cfg_file->read('app_user_password');
+    print "<br>";
+    print $cfg_file->read('app_database');
 ?>
